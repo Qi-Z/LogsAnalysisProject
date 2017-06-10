e8e6a0ccf609db814b8beddcf669086e0ed468f7
qizhang4 <qizhang4@illinois.edu>
2017-06-10 09:54:10 -0400

    Finish report 2

--- vagrant/main.py
+++ vagrant/main.py
@@ -22,4 +22,15 @@ if __name__ == "__main__":
     for article in results1:
         print("\"{}\" --- {} views".format(article[1], article[0]))
 
+    query2 = """SELECT COUNT(log_id), authors.name
+                FROM authors INNER JOIN
+                (SELECT articles.id AS article_id, log.id AS log_id, author
+                FROM articles INNER JOIN log 
+                ON CONCAT('/article/', articles.slug)=log.path) AS article_log
+                ON article_log.author=authors.id
+                GROUP BY authors.name
+                ORDER BY COUNT(log_id) DESC;"""
+    results2 = execute_query(cur, query2).fetchall()
+    for author in results2:
+        print("{} --- {} views".format(author[1], author[0]))
 
